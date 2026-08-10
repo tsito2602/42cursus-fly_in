4f9783da4cac216a67a8491fdb6e4d704ff4f623
tsito2602 <white.galaxys2.album2@gmail.com>
2026-08-10 12:21:11 +0900

    main.pyを追加、fly-inコマンドを定義

---
+++ src/fly_in/main.py
@@ -0,0 +1,21 @@
+from pydantic import ValidationError
+from fly_in.parsing.map_parser import (
+    MapParser,
+    get_map_file,
+    ParsingError,
+)
+
+
+def main() -> None:
+    file = get_map_file()
+    map_parser = MapParser(file)
+
+    try:
+        map = map_parser.load()
+        print(map)
+    except (ParsingError, ValidationError) as error:
+        print(error)
+
+
+if __name__ == "__main__":
+    main()
